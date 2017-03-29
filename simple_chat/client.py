8fb3fa8c2fc919196d0a3dfb49238f3dd291e34d
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2017-03-29 22:37:06 +0200

    adding a little bit of gui

--- simple_chat/client.py
+++ simple_chat/client.py
@@ -1,33 +1,92 @@
+from tkinter import *
 import rpyc
+import queue
+from queue import Queue
+from queue import Empty
 
 c = rpyc.connect('localhost', 18812)
 bgsrv = rpyc.BgServingThread(c)
 
-
-def get_msg(s):
-    """function to receiving messages"""
-    print(s)
-
-def ping():
-    """ping pong he"""
-    # kop bitcoiny
-    return 'pong'
+class GUI:
 
 
-communication = {
-    'ping': ping,
-    'get_msg': get_msg
-}
+    def __init__(self, username):
+        self.to_read = Queue()
+        # self.to_read.put(1122)
+        # print(self.to_read.get(block=False))
+        self.root = Tk()
+        self.conn = None
 
-username = input('Your username: ')
+        self.root.title(username)
+        # self.conn = self.exposed_conn
+        self.exposed_username = username
 
-# class for interacting with server
-myself = c.root.connect(username, communication)
+        self.box = Text(self.root, width=30, height=10)
+        self.box.pack()
 
-while True:
-    where = input('To who: ')
-    what = input('What: ')
-    myself.send_msg(where, what)
+        Label(self.root, text='Username').pack(fill=X)
+        self.input_where = Entry(self.root)
+        self.input_where.pack(fill=X)
+        Label(self.root, text='What to send').pack(fill=X)
+        self.input_what = Entry(self.root)
+        self.input_what.pack(fill=X)
+
+        Button(self.root, text='Wyslij', command=self.xd).pack()
+
+        self.display_data()
+
+    def exposed_get_msg(self, msg):
+
+        # msg = '[{who}]:{what}'.format(msg['who', msg['what']])
+        self.to_read.put(msg)
+
+    def send_msg(self):
+        where = self.input_where.get()
+        what = self.input_what.get()
+        self.conn.send_msg(where, what)
+
+    def display_data(self):
+        while True:
+            try:
+                message = self.to_read.get(block=False)
+            except Empty:
+                break
+            else:
+
+                self.box.insert(END, '%s\n' % message)
+        self.root.after(100, self.display_data)
+
+    def get_information(self):
+        info = {}
+        info['username'] = self.exposed_username
+        info['get_msg'] = self.exposed_get_msg
+        return info
+
+    def xd(self):
+        self.conn.server.send_msg(self, '123', 'siemka')
+username = input('Username: ')
+
+gui = GUI(username)
+# information = {'username': gui.exposed_username,
+#                'get_msg': gui.exposed_get_msg}
+gui.conn = c.root.connect(gui.get_information())
+gui.root.mainloop()
+#
+# username = input('Your username: ')
+#
+# # class for interacting with server
+
+#
+# while True:
+#     where = input('To who: ')
+#     what = input('What: ')
+#     myself.send_msg(where, what)
+#
+
+
+
+# root.protocol('WM_DELETE_WINDOW', on_closing)
+
 
 bgsrv.stop()
 c.close()
--- simple_chat/moj_serwer.py
+++ simple_chat/moj_serwer.py
@@ -1,29 +1,47 @@
 import rpyc
+from tkinter import *
 
 class UsernameTaken(Exception): pass
 class InvalidCommunication(Exception): pass
+class UsernameNotFound(Exception): pass
+class HackDetected(Exception):pass
+
+class Validate:
+    @staticmethod
+    def check_client(info):
+        data = {}
+        try:
+            data['username'] = info['username']
+            data['get_msg'] = info['get_msg']
+            if not not data['username'] or not data['username'].isalnum():
+                raise AttributeError
+        except AttributeError as ee:
+            raise AttributeError("Sorry you don't have :", ee)
+        if type(data['username']) != str:  # sorry for type checking :/
+            raise AttributeError('Username is not a string')
+        return data
 
 class User:
     """Instance is given to user as a interface
     Server can invoke get_msg and ping"""
-    def __init__(self, connection=None, name=None, communication=None):
+    def __init__(self, connection=None, username=None, get_msg=None):
         assert connection is not None, 'Who are you?'
-        assert name is not None, 'Whats your name?'
-        assert name.isalnum() is True, 'Your name needs to be allphanumerical'
+
 
         self.server = server  # server
         self.connection = str(connection)  # connection
-        self.name = name  # username
+        self.name = username  # username
         try:
-            self.get_msg = communication['get_msg']  # function for sending msg
+            self.get_msg = get_msg  # function for sending msg
             # self.ping = communication['ping']  # function for pinging
         except KeyError:
             msg = """No communication functions found\n
              send {'get_msg': retrieve, 'ping': ping}"""
             raise InvalidCommunication(msg)
 
-
     def exposed_send_msg(self, where, what):
+        if self.server.check_user(self):
+            raise HackDetected('Are You Really Who You Think You Are?')
         self.server.send_msg(self, where, what)
 
     def __str__(self):
@@ -45,7 +63,7 @@ class Server:
         """invokes 'send_msg' function in chosen user"""
         where = self.get_user(where)
         if where:
-            where.get_msg('[{who}]: {what}'.format(who=who, what=what))
+            where.get_msg({'who': str(who), 'what': str(what)})
 
     def get_user(self, name=None, conn=None):
         """Retrieves user object basing on username or connection"""
@@ -66,13 +84,23 @@ class Server:
         else:
             raise UsernameTaken('Username already taken. Choose another')
 
+    def check_user(self, user):
+        username = self.get_user(name=user.username)
+        connection = self.get_user(conn=user.connection)
+
+        if(username is connection) and username is not False:
+            return True
+        return False
+
     def remove_user(self, name=None, conn=None):
         """Remove user basing on connection """
         user = self.get_user(name, conn)  # find user
         if user:
             # delete
+            self.show_users()  # show users
             del self.connections[repr(user)]
             del self.usernames[str(user)]
+
         else:
             print('Someone disconnected without logging in')
 
@@ -81,17 +109,22 @@ class Server:
         del self.connections
         del self.usernames
 
+    def show_users(self):
+        print('Connected users:', list(server.usernames.keys()))
 
 class ChatService(rpyc.SlaveService):
 
-    def exposed_connect(self, *args, **kwargs):
+    def exposed_connect(self, info):
         """Function returning interface for sending messages"""
         connection = self.exposed_getconn()  # identifying connection
-        new_user = User(connection, *args, **kwargs)  # makes new user
+        client_data = Validate.check_client(info)
+        print(client_data)
+        new_user = User(connection, **client_data)  # makes new user
         if server.add_user(new_user):
             # user can be added
             print('Connected users:', list(server.usernames.keys()))
-            return new_user  # returning interface
+            # client.conn = new_user  # oh cant do this
+            return new_user
         else:
             return False
 
@@ -99,11 +132,12 @@ class ChatService(rpyc.SlaveService):
     def on_connect(self):
         pass
 
+
     def on_disconnect(self):
         # removing disconnected user
         conn = self.exposed_getconn()
         server.remove_user(conn=conn)
-        print('Connected users:', list(server.usernames.keys()))
+
 
 
 if __name__ == "__main__":
